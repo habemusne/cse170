7f65b6d609abf00fc1e4088a239bbae03355fce0
Xiangcheng Zhu <zhu788_588@hotmail.com>
2014-02-06 20:41:30 -0800

    first

---
+++ thumbnail.js
@@ -0,0 +1,15 @@
+function thumbnail() { 
+	const NAVPANEL_COLOR = 'rgba(100, 100, 100, 0.2)';    // 导航栏背景色 
+	const NAVBUTTON_BACKGROUND = 'rgb(40, 40, 40)';  // 导航栏中 button 的背景色			
+	const NAVBUTTON_COLOR = 'rgb(255, 255, 255)';   //button 的前景色
+	const NAVBUTTON_HL_COLOR = 'rgb(100, 100, 100)';   //button 高亮时的前景色
+	var canvas = document.getElementById('canvas');   // 获得 canvas 对象   
+	var context = canvas.getContext('2d');    // 获得上下文对象   	
+	this.load = function()
+	}
+ }
+ 
+ window.onload = function() { 
+	 thumb = new thumbnail();
+	 thumb.load();
+ }
